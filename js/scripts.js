e9ff70cb4ed719993bf37a0caa0822d3b110d8ea
luistenorio2024 <luis.tenorio@ucn.edu.ni>
2024-06-22 11:24:47 -0600

    carga completa
    carga completa

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,13 @@
-// Espera a que el DOM se haya cargado completamente
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM completamente cargado y analizado');
-
-    // Agrega un evento de clic a cada enlace de navegación
+document.addEventListener('DOMContentLoaded', function( {
+    console.log('DOM completamente cargado y analizado);
     var navLinks = document.querySelectorAll('nav ul li a');
-    navLinks.forEach(function(link) {
+    navLinks.forEach(function(link {
         link.addEventListener('click', function(event) {
             alert('Has hecho clic en un enlace!');
         });
-    });
-
-    // Agrega un evento de envío al formulario de contacto
+    }));
     document.querySelector('form').addEventListener('submit', function(event) {
-        event.preventDefault(); // Previene el envío del formulario
+        event.preventDefault();
         alert('Formulario enviado');
     });
 });
